01add6808c53f213579e8b7442c801024cdf3679
Stephane Nicoll <snicoll@pivotal.io>
2017-01-18 12:10:09 +0100

    Add sample controller

--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -1,7 +1,14 @@
 package com.example.web;
 
+import com.example.domain.User;
 import com.example.domain.UserRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +18,21 @@ public class UserController {
 
 	public UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+
+	@GetMapping("/users/{id}")
+	Mono<User> getById(@PathVariable String id) {
+		return this.userRepository.findOne(id);
+	}
+
+	@GetMapping("/users")
+	Flux<User> all() {
+		return this.userRepository.findAll();
+	}
+
+	@PostMapping("/users")
+	Mono<Void> save(@RequestBody Mono<User> user) {
+		return this.userRepository.save(user).then();
 	}
 
 }
